0008ae83ccb618ea594556bc700d09ce9fcca7bd
wangtianyu <wangtianyu>
2018-05-02 18:00:26 +0800

    123

---
+++ src/store/movies/api.js
@@ -0,0 +1,17 @@
+import fetch from './../base';
+
+export const MOVIESTYPE = {
+  inTheaters: 'in_theaters',
+  comingSoon: 'coming_soon',
+};
+
+export function fetchMovies(type, { city = '广州', start = 0 } = {}) {
+  return fetch(`movie/${type}`, { city, start });
+}
+export function fetchMoviesQuery({ q, start = 0 }) {
+  console.log('fetchMoviesQuery');
+  return fetch('/movie/search', { q, start });
+}
+export function fetchMovieSubject(id) {
+  return fetch(`movie/subject/${id}`);
+}
